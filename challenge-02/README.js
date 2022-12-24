2388825807d3fdce51b03a04545ebb26cb228f66
brnowmar <bruno.mar.rj@gmail.com>
2022-12-24 18:08:05 -0300

    hello

--- challenge-02/README.js
+++ challenge-02/README.js
@@ -1,32 +1,39 @@
-# Desafio da semana #2
 
-Nesse exercício, você está livre para escolher os nomes para suas variáveis e funções! :smile:
 
-```js
+
+
+
 // Crie uma função que receba dois argumentos e retorne a soma dos mesmos.
-?
+function carzy(argumentOne, ArgumentTwo){
+    return argumentOne + ArgumentTwo;
+};
 
 // Declare uma variável que receba a invocação da função criada acima, passando dois números quaisquer por argumento, e somando `5` ao resultado retornado da função.
-?
+let newVar = carzy(10,2) + 5;
 
-// Qual o valor atualizado dessa variável?
-?
+// Qual o valor atualizado dessa variável
+console.log(newVar)
 
 // Declare uma nova variável, sem valor.
-?
+let anonVar;
 
 /*
 Crie uma função que adicione um valor à variável criada acima, e retorne a string:
     O valor da variável agora é VALOR.
 Onde VALOR é o novo valor da variável.
 */
-?
+function newFunction(newValor){
+    newValor = 'VALOR'
+    return 'O valor da variável agora é ' + newValor
+};
+anonVar = newFunction();
 
 // Invoque a função criada acima.
-?
 
-// Qual o retorno da função? (Use comentários de bloco).
-?
+console.log(anonVar)
+// Qual o retorno da função (Use comentários de bloco).
+/* O retorno da funcção é:
+'O valor da variável agora é VALOR, onde VALOR é uma variavel dentro da função newvar e pode ser trocada */ 
 
 /*
 Crie uma função com as seguintes características:
@@ -35,19 +42,26 @@ Crie uma função com as seguintes características:
     Preencha todos os valores corretamente!
 3. O retorno da função deve ser a multiplicação dos 3 argumentos, somando `2` ao resultado da multiplicação.
 */
-?
+function crazeFunction(valorOne, valorTwo, valorThree){
+    if (!valorOne ||!valorTwo || !valorThree){
+        return console.log('Preencha todos os valores corretamente!')
+    }else{
+        return (valorOne * valorTwo * valorThree) + 2;
+    }
+    
+};
 
 // Invoque a função criada acima, passando só dois números como argumento.
-?
+console.log(crazeFunction(2,4))
 
-// Qual o resultado da invocação acima? (Use comentários para mostrar o valor retornado).
-?
+// Qual o resultado da invocação acima (Use comentários para mostrar o valor retornado).
+//'Preencha os valores corretamente.'
 
 // Agora invoque novamente a função criada acima, mas passando todos os três argumentos necessários.
-?
+console.log(crazeFunction (2,4,6))
 
-// Qual o resultado da invocação acima? (Use comentários para mostrar o valor retornado).
-?
+// Qual o resultado da invocação acima (Use comentários para mostrar o valor retornado).
+//valor retornaro : 50;
 
 /*
 Crie uma função com as seguintes características:
@@ -58,8 +72,28 @@ Crie uma função com as seguintes características:
 5. Se nenhum argumento for passado, retorne o valor booleano `false`.
 6. E ainda, se nenhuma das condições acima forem atendidas, retorne `null`.
 */
-?
+function testing(a, b, c){
+    if (a !== undefined && b === undefined && c === undefined){
+        return a;
+    }
+    else if (a !== undefined && b !== undefined && c === undefined){
+        return a + b;
+    }
+    else if (a !== undefined && b !== undefined && c !== undefined){
+        return (a+b)/c;
+    }
+    else if (a === undefined && b === undefined && c === undefined){
+        return false;
+    } else {
+        return null;
+    }
+        
+    
+}
 
 // Invoque a função acima utilizando todas as possibilidades (com nenhum argumento, com um, com dois e com três.) Coloque um comentário de linha ao lado da função com o resultado de cada invocação.
-?
-```
+
+console.log(testing())
+console.log(testing(5))
+console.log(testing(5,10))
+console.log(testing(5,10,15))
--- challenge-03/README.js
+++ challenge-03/README.js
@@ -1,9 +1,7 @@
-# Desafio da semana #3
 
-```js
 // Declarar uma variável qualquer, que receba um objeto vazio.
-?
 
+let myNewVary = {};
 /*
 Declarar uma variável `pessoa`, que receba suas informações pessoais.
 As propriedades e tipos de valores para cada propriedade desse objeto devem ser:
@@ -16,14 +14,63 @@ As propriedades e tipos de valores para cada propriedade desse objeto devem ser:
 - `andando` - Boolean - recebe "falso" por padrão
 - `caminhouQuantosMetros` - Number - recebe "zero" por padrão
 */
-?
+let pessoa = {
+    nome : 'Bruno',
+    sobrenome : 'silva',
+    sexo : 'masculino',
+    idade : 25,
+    altura : 1.70,
+    peso : 60,
+    andando : false,
+    caminhouQauntosMetros : 10,
+    fazerAniversario : function(){
+        return pessoa.idade += 1;
+    },
+    andar : function(metrosAcaminhar){
+        pessoa.andando = true;
+        return pessoa.caminhouQauntosMetros += metrosAcaminhar;
+    },
+    parar : function(){
+        return pessoa.andando = false;
+    },
+    nomeCompleto : function(){
+        return 'Olá! Meu nome é ' + pessoa.nome + ' ' + pessoa.sobrenome + '!'
+    },
+    mostrarIdade : function(){
+        return 'Olá, eu tenho '+ pessoa.idade +' anos!' ;
+    },
+    mostrarAltura : function(){
+        return 'Minha altura é '+pessoa.altura+'m.';
+    },
+    mostrarPeso : function(){
+        return 'Eu peso ' + pessoa.peso + 'Kg.';
+    },
+    apresentacao : function(){
+        let pluralDaIdade = 'anos';
+        if(pessoa.idade <= 0){
+            pluralDaIdade = 'ano'
+        }; 
+        let pluralDosMetros = 'metros';
+        if(pessoa.caminhouQauntosMetros <= 0 ){
+            pluralDosMetros = 'metro'
+        }
+        let sexoDaSentenca = 'a';
+        if(pessoa.sexo === 'masculino'){
+            sexoDaSentenca = 'o'
+        }
+        return 'Olá, eu sou ' + sexoDaSentenca +' '+ pessoa.nome + ' ' + pessoa.sobrenome +', tenho '+ pessoa.idade + ' ' + pluralDaIdade + ', '+ pessoa.altura + 'm, meu peso é ' + pessoa.peso+'kg e, só hoje, eu já caminhei '+ pessoa.caminhouQauntosMetros +' '+ pluralDosMetros + '!';
+        
+    }
+
+
+}
 
 /*
 Adicione um método ao objeto `pessoa` chamado `fazerAniversario`. O método deve
 alterar o valor da propriedade `idade` dessa pessoa, somando `1` a cada vez que
 for chamado.
 */
-?
+
 
 /*
 Adicione um método ao objeto `pessoa` chamado `andar`, que terá as seguintes
@@ -35,103 +82,103 @@ valor dessa propriedade a quantidade passada por parâmetro;
 - Ele deverá modificar o valor da propriedade `andando` para o valor
 booleano que representa "verdadeiro";
 */
-?
+
 
 /*
 Adicione um método ao objeto `pessoa` chamado `parar`, que irá modificar o valor
 da propriedade `andando` para o valor booleano que representa "falso".
 */
-?
+
 
 /*
 Crie um método chamado `nomeCompleto`, que retorne a frase:
 - "Olá! Meu nome é [NOME] [SOBRENOME]!"
 */
-?
+
 
 /*
 Crie um método chamado `mostrarIdade`, que retorne a frase:
 - "Olá, eu tenho [IDADE] anos!"
 */
-?
+
 
 /*
 Crie um método chamado `mostrarPeso`, que retorne a frase:
 - "Eu peso [PESO]Kg."
 */
-?
+
 
 /*
 Crie um método chamado `mostrarAltura` que retorne a frase:
 - "Minha altura é [ALTURA]m."
 */
-?
+
 
 /*
 Agora vamos brincar um pouco com o objeto criado:
-Qual o nome completo da pessoa? (Use a instrução para responder e comentários
+Qual o nome completo da pessoa (Use a instrução para responder e comentários
 inline ao lado da instrução para mostrar qual foi a resposta retornada)
 */
-?
+console.log(pessoa.nomeCompleto())
 
 /*
-Qual a idade da pessoa? (Use a instrução para responder e comentários
+Qual a idade da pessoa (Use a instrução para responder e comentários
 inline ao lado da instrução para mostrar qual foi a resposta retornada)
 */
-?
+console.log(pessoa.mostrarIdade())// como podemos ver tenho 25 anos kkkkk
 
 /*
-Qual o peso da pessoa? (Use a instrução para responder e comentários
+Qual o peso da pessoa (Use a instrução para responder e comentários
 inline ao lado da instrução para mostrar qual foi a resposta retornada)
 */
-?
 
+console.log(pessoa.mostrarPeso())// deu certo
 /*
-Qual a altura da pessoa? (Use a instrução para responder e comentários
+Qual a altura da pessoa (Use a instrução para responder e comentários
 inline ao lado da instrução para mostrar qual foi a resposta retornada)
 */
-?
+console.log(pessoa.mostrarAltura())
 
 /*
 Faça a `pessoa` fazer 3 aniversários.
 */
-?
+//é só invocar no navegador pessoa.fazerAniversario() 3 vezes
 
 /*
-Quantos anos a `pessoa` tem agora? (Use a instrução para responder e
+Quantos anos a `pessoa` tem agora (Use a instrução para responder e
 comentários inline ao lado da instrução para mostrar qual foi a resposta
 retornada)
 */
-?
+//resposta :  28
 
 /*
 Agora, faça a `pessoa` caminhar alguns metros, invocando o método `andar` 3x,
 com metragens diferentes passadas por parâmetro.
 */
-?
+//resposta : pessoa.andar(10) pessoa.andar(10) pessoa.andar(10)
 
 /*
-A pessoa ainda está andando? (Use a instrução para responder e comentários
+A pessoa ainda está andando (Use a instrução para responder e comentários
 inline ao lado da instrução para mostrar qual foi a resposta retornada)
 */
-?
+//pessoa.andando() resposta true 
 
 /*
 Se a pessoa ainda está andando, faça-a parar.
 */
-?
+//pessoa.parar();
 
 /*
-E agora: a pessoa ainda está andando? (Use uma instrução para responder e
+E agora: a pessoa ainda está andando (Use uma instrução para responder e
 comentários inline ao lado da instrução para mostrar a resposta retornada)
 */
-?
+// deu certo funciona o codigo e a pessoa para
 
 /*
-Quantos metros a pessoa andou? (Use uma instrução para responder e comentários
+Quantos metros a pessoa andou (Use uma instrução para responder e comentários
 inline ao lado da instrução para mostrar a resposta retornada)
 */
-?
+//30 metros
 
 /*
 Agora vamos deixar a brincadeira um pouco mais divertida! :D
@@ -150,8 +197,8 @@ deve conter no retorno da frase acima é "metro" no lugar de "metros".
 método), que será concatenada com a frase de retorno, mostrando a resposta
 correta, de acordo com os dados inseridos no objeto.
 */
-?
+
 
 // Agora, apresente-se ;)
-?
-```
+console.log(pessoa.apresentacao())
+
